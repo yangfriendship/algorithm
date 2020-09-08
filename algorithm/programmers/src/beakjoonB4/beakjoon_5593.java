fcbf7373cc69623cd7f47eddccdbaa5cff09019a
youzheng <yangfriendship@naver.com>
2020-09-09 03:26:47 +0900

    2020.09.08_2

--- algorithm/programmers/src/beakjoonB4/beakjoon_5593.java
+++ algorithm/programmers/src/beakjoonB4/beakjoon_5593.java
@@ -7,28 +7,57 @@ import java.util.StringTokenizer;
 import java.util.StringTokenizer;
 
 public class beakjoon_5593 {
-	
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String input = "10110111";
-		String seventeen = "10111";
+		String input = br.readLine();
 
-		String[] result = new String[input.length()+seventeen.length()+1]; 
-		
-		int cnt = 0;
-		
-		for (int i = 0; i < input.length(); i++) {
-			
-			for (int j = 0; j < seventeen.length(); j++) {
-				
-				result[]
-				
-			}
-			
+		String addShiliu = input + "0000";
+
+		int[] addShiliu2 = new int[addShiliu.length()+1];
+		int[] input2 = new int[input.length()];
+
+		for (int i = 0; i < addShiliu2.length; i++) {
+			if(i==addShiliu.length()) break;
+			addShiliu2[i] = addShiliu.charAt(addShiliu.length() - 1 - i) - '0';
 		}
-		
-		
+		for (int i = 0; i < input2.length; i++) {
+			input2[i] = input.charAt(input.length() - i - 1) - '0';
+		}
+		for (int i = 0; i < input2.length; i++) {
+
+			addShiliu2[i] += input2[i];
+
+		}
+
+		for (int i = 0; i < addShiliu2.length; i++) {
+
+			switch (addShiliu2[i]) {
+			case 0:
+				addShiliu2[i] = 0;
+				break;
+			case 1:
+				addShiliu2[i] = 1;
+				break;
+			case 2:
+				addShiliu2[i] = 0;
+				addShiliu2[i + 1] += 1;
+				break;
+			case 3:
+				addShiliu2[i] = 1;
+				addShiliu2[i + 1]++;
+				break;
+			}
+		}
+
+		for (int i = addShiliu2.length - 1; i >= 0; i--) {
+			if(i==addShiliu2.length-1 && addShiliu2[i]==0) {
+				continue;
+			}
+			System.out.print(addShiliu2[i]);
+		}
+
 	}
 }
