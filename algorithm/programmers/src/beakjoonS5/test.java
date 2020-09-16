c0c75e15c3575cadfc552142f54c6a539f484232
youzheng <yangfriendship@naver.com>
2020-09-17 04:21:48 +0900

    20.09.16

--- algorithm/programmers/src/beakjoonS5/test.java
+++ algorithm/programmers/src/beakjoonS5/test.java
@@ -5,14 +5,26 @@ import java.util.Scanner;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
 class test {
+	
+
+	public static String reserve(String str) {
+
+		char temp;
+		char[] c = str.toCharArray();
+		int lastIndex = (str.length() / 2);
+		for (int i = 0; i < lastIndex; i++) {
+			temp = c[i];
+			c[i] = c[c.length - 1 - i];
+			c[c.length - 1 - i] = temp;
+		}
+		return new String(c);
+	}
 
 	public static void main(String[] args) {
 
-		String a = "a";
-		String b = "b";
-		String c = "c";
-
-		System.out.println(a.compareTo(b));
+		String str = "ef gh";
+		
+		System.out.println(reserve(str));
 		
 		
 	}
