01c8808ab77dcf0eb39d526a3c88c7839e9f365f
youzheng <yangfriendship@naver.com>
2020-09-24 22:52:18 +0900

    beakjoon_2798

--- algorithm/programmers/src/beakjoon/test.java
+++ algorithm/programmers/src/beakjoon/test.java
@@ -5,27 +5,14 @@ import java.util.Scanner;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
 class test {
-	
-
-	public static String reserve(String str) {
-
-		char temp;
-		char[] c = str.toCharArray();
-		int lastIndex = (str.length() / 2);
-		for (int i = 0; i < lastIndex; i++) {
-			temp = c[i];
-			c[i] = c[c.length - 1 - i];
-			c[c.length - 1 - i] = temp;
-		}
-		return new String(c);
-	}
 
 	public static void main(String[] args) {
 
-		String str = "ef gh";
-		
-		System.out.println(reserve(str));
+
+		System.out.println(Math.cbrt(100));
+		System.out.println(Math.cbrt(100)==(int)Math.cbrt(100));
 		
 		
 	}
+
 }
