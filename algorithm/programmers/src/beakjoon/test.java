a817706a3bdc40afcadc8109632b4d1cf3beb500
youzheng <yangfriendship@naver.com>
2020-09-27 07:17:12 +0900

    beakjoon_11725

--- algorithm/programmers/src/beakjoon/test.java
+++ algorithm/programmers/src/beakjoon/test.java
@@ -1,18 +1,13 @@
 package beakjoon;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
 class test {
 
 	public static void main(String[] args) {
 
 
-		System.out.println(Math.cbrt(100));
-		System.out.println(Math.cbrt(100)==(int)Math.cbrt(100));
-		
-		
-	}
-
+}
 }
