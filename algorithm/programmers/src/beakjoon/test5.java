a3af7e69c400cc52dbba8ec722e190b456147cfe
youzheng <yangfriendship@naver.com>
2020-10-04 13:40:08 +0900

    beakjoon_1080

--- algorithm/programmers/src/beakjoon/test5.java
+++ algorithm/programmers/src/beakjoon/test5.java
@@ -1,48 +1,28 @@
 package beakjoon;
 
-class Person {
-	private int age;
-	private String name;
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
+class Cl{
+	
+	static String name;
+	
+	static void setName(String name) {
 		this.name = name;
 	}
-	
-	void print() {
-		System.out.println("ok");
+	static String getName() {
+		return this.name;
 	}
+	
+	static void say() {
+		System.out.println(name);
+	}
+	
 }
 
 public class test5 {
 
-	public static void met(Person ps) {
-		ps.setAge(100);
-		ps.setName("¿ìÁ¤");
-	}
+public static void main(String[] args) {
 	
-	public void stringMet(String str) {
-		str = str + "okok";
-		System.out.println(str);
-	}
-
-	public static void main(String[] args) {
-		
-		new Person().print();
-//		Person.print();
-		
-		
-		
-	}
+	Cl.say();
+	Cl.se
+	
 }
+	}
