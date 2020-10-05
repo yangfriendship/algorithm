8b0eda53712b6f8af6724cf57c46740fb4661e90
youzheng <yangfriendship@naver.com>
2020-10-05 13:37:36 +0900

    beakjoon_16953

--- algorithm/programmers/src/beakjoon/test5.java
+++ algorithm/programmers/src/beakjoon/test5.java
@@ -1,28 +1,12 @@
 package beakjoon;
 
-class Cl{
-	
-	static String name;
-	
-	static void setName(String name) {
-		this.name = name;
-	}
-	static String getName() {
-		return this.name;
-	}
-	
-	static void say() {
-		System.out.println(name);
-	}
-	
-}
-
 public class test5 {
 
-public static void main(String[] args) {
-	
-	Cl.say();
-	Cl.se
-	
-}
+	public static void main(String[] args) {
+
+		int a = 111111105;
+		
+		System.out.println(a/10);
+		
 	}
+}
