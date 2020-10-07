ac15930b1a61a6314abdac60ae44e24ffbaab2e9
youzheng <yangfriendship@naver.com>
2020-10-07 14:05:01 +0900

    beakjoon_12904

--- algorithm/programmers/src/beakjoon/test5.java
+++ algorithm/programmers/src/beakjoon/test5.java
@@ -1,21 +1,18 @@
 package beakjoon;
 
-import java.util.Collections;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Stack;
-
 public class test5 {
 	public static void main(String[] args) {
 
-		Deque<Integer> que = new LinkedList<Integer>();
-		que.add(0);
-		que.addLast(1);
-		que.addLast(1);
-		que.addLast(1);
-		que.add(2);
-		que.addFirst(9);
-		que.pollFirst();
-		System.out.println(que);
+		StringBuffer sb = new StringBuffer();
+		sb.append("woojung");
+		String s = "woojung";
+		char[] arr = s.toCharArray();
+		System.out.println(arr);
+		System.out.println(sb);
+		System.out.println(sb.equals(arr));
+		System.out.println(sb.equals(s));
+		System.out.println(s.equals(sb));
+		
+		
 	}//main
 }
