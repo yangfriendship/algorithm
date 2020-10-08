dae17e3ecaceb42fb4cda505516764ce8527d121
youzheng <yangfriendship@naver.com>
2020-10-08 13:50:18 +0900

    beakjoon_14592

--- algorithm/programmers/src/beakjoon/test5.java
+++ algorithm/programmers/src/beakjoon/test5.java
@@ -1,21 +1,32 @@
 package beakjoon;
 
-import java.util.LinkedList;
-import java.util.List;
+class Person{
+	private static int age;
+	int number =0;
+	
+	public static int getAge() {
+		return age;
+	}
 
+
+	public static void setAge(int age) {
+		Person.age = age;
+	}
+
+
+	public static void printAge() {
+		System.out.println();
+	}
+}
 public class test5 {
+	
+
+	
 	public static void main(String[] args) {
 
-		List<Integer> list = new LinkedList<Integer>();
-		
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		System.out.println(list);
-		list.add(list.size(),list.get(1));
-		System.out.println(list);
-		list.remove(1);
-		System.out.println(list);
+		Person ps = new Person();
+		ps.setAge(10);
+		ps.printAge();
 		
 		
 	}//main
