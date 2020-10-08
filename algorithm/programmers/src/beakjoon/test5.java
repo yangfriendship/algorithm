8f6fb034bc89b506bb91803b2374702e4e7d044b
youzheng <yangfriendship@naver.com>
2020-10-09 02:54:39 +0900

    beakjoon_1302

--- algorithm/programmers/src/beakjoon/test5.java
+++ algorithm/programmers/src/beakjoon/test5.java
@@ -1,32 +1,11 @@
 package beakjoon;
-
-class Person{
-	private static int age;
-	int number =0;
-	
-	public static int getAge() {
-		return age;
-	}
-
-
-	public static void setAge(int age) {
-		Person.age = age;
-	}
-
-
-	public static void printAge() {
-		System.out.println();
-	}
-}
 public class test5 {
-	
-
-	
 	public static void main(String[] args) {
 
-		Person ps = new Person();
-		ps.setAge(10);
-		ps.printAge();
+		String a = "a";
+		String ab = "ab";
+		
+		System.out.println(a.compareTo(ab));
 		
 		
 	}//main
