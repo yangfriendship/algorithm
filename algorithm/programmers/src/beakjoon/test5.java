2ded325bef093c1ef4de91785de210ae443f43e5
youzheng <yangfriendship@naver.com>
2020-10-09 18:25:20 +0900

    beakjoon_9012

--- algorithm/programmers/src/beakjoon/test5.java
+++ algorithm/programmers/src/beakjoon/test5.java
@@ -1,24 +1,21 @@
 package beakjoon;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class test5 {
 	public static void main(String[] args) {
 
 		
-		List<Integer> list = new ArrayList<Integer>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(4);
-		list.add(5);
-		list.add(1);
-		list.add(2);
+		System.out.println((int)'(');
+		System.out.println((int)')');
 		
-		System.out.println(list.indexOf(1));
-		
-		
+		Stack<Integer> stk = new Stack<Integer>();
+		stk.add(1);
+		stk.add(2);
+		System.out.println(stk.peek());
 	}//main
 }
