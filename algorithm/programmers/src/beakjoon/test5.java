716c1070e9701c37f0dcf92d9dd64cd9a17ea789
youzheng <yangfriendship@naver.com>
2020-10-09 15:54:02 +0900

    beakjoon_2910

--- algorithm/programmers/src/beakjoon/test5.java
+++ algorithm/programmers/src/beakjoon/test5.java
@@ -1,16 +1,23 @@
 package beakjoon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class test5 {
 	public static void main(String[] args) {
 
-		char a = '(';
-		char b = ')';
-		char c = '[';
-		char d = ']';
 		
-		System.out.println((int)a);
-		System.out.println((int)b);
-		System.out.println((int)c);
-		System.out.println((int)d);
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(1);
+		list.add(2);
+		
+		System.out.println(list.indexOf(1));
 		
 		
 	}//main
