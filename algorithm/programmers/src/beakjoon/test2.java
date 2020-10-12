3901add9a1e67a381dfc158dad94cc54a32a3efa
youzheng <yangfriendship@naver.com>
2020-10-12 23:46:02 +0900

    beakjoon_5397

--- algorithm/programmers/src/beakjoon/test2.java
+++ algorithm/programmers/src/beakjoon/test2.java
@@ -7,21 +7,10 @@ public class test2 {
 
 	public static void main(String[] args) {
 
+		int a = 0;
+		String str = "";
 		
-		int[] arr = new int[10];
-		System.out.println(arr[1]);
-		test(arr);
-		System.out.println(arr[1]);
-		test(arr);
-
-	}
-	
-	public static void test(int[] arr) {
-		if(arr[1]==5) {
-			System.out.println("5юс");
-		}
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = 5;
-		}
+		
+		
 	}
 }
