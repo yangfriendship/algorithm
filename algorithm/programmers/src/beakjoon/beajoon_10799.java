227b1a0a6ec9c299c7416b8c7f923b379145f39b
youzheng <yangfriendship@naver.com>
2020-10-13 18:16:00 +0900

    beakjoon_11279

--- algorithm/programmers/src/beakjoon/beajoon_10799.java
+++ algorithm/programmers/src/beakjoon/beajoon_10799.java
@@ -25,6 +25,7 @@ public class beajoon_10799 {
 					count++;
 				}
 			}
+			
 		}
 		System.out.println(count);
 	}
--- algorithm/programmers/src/beakjoon/test2.java
+++ algorithm/programmers/src/beakjoon/test2.java
@@ -1,16 +1,21 @@
 package beakjoon;
 
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class test2 {
 
 	public static void main(String[] args) {
 
-		int a = 0;
-		String str = "";
-		
-		
+		PriorityQueue<Integer> que = new PriorityQueue<Integer>();
+		que.add(1);
+		que.add(2);
+		que.add(3);
+		que.add(4);
+		System.out.println(que);
+		que.poll();
+		System.out.println(que);
 		
 	}
 }
