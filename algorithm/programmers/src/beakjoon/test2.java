bec9ec35fde6634d55cd85e8b59899f2ba9aabba
youzheng <yangfriendship@naver.com>
2020-10-15 19:04:05 +0900

    beakjoon_1213

--- algorithm/programmers/src/beakjoon/test2.java
+++ algorithm/programmers/src/beakjoon/test2.java
@@ -8,14 +8,13 @@ public class test2 {
 
 	public static void main(String[] args) {
 
-		PriorityQueue<Integer> que = new PriorityQueue<Integer>();
-		que.add(1);
-		que.add(2);
-		que.add(3);
-		que.add(4);
-		System.out.println(que);
-		que.poll();
-		System.out.println(que);
 		
+		System.out.println((int)'A'-65);
+
+		char c = (char)(65);
+		System.out.println(c);
 	}
+	
+	
+	
 }
