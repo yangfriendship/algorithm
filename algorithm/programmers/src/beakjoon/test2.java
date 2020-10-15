7e4f035adc747cd6daaea54e789100120d55fb15
youzheng <yangfriendship@naver.com>
2020-10-15 21:03:18 +0900

    beakjoon_16968

--- algorithm/programmers/src/beakjoon/test2.java
+++ algorithm/programmers/src/beakjoon/test2.java
@@ -1,20 +1,38 @@
 package beakjoon;
 
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Scanner;
 
 public class test2 {
-
 	public static void main(String[] args) {
-
-		
-		System.out.println((int)'A'-65);
-
-		char c = (char)(65);
-		System.out.println(c);
+		Scanner sc = new Scanner(System.in);
+		char[] carN = sc.next().toCharArray();
+		int ans = 1;
+		for (int i = 0; i < carN.length; i++) {
+			if (i == 0) { 
+            // 첫번째 숫자의 경우의 수
+				if (carN[i] == 'c') {
+					ans *= 26;
+				} else {
+					ans *= 10;
+				}
+			} else {
+            //두번째 숫자부터의 경우의 수
+				if (carN[i] == carN[i - 1]) 
+					if (carN[i] == 'c') {
+						ans *= 25;
+					} else {
+						ans *= 9;
+					}
+				}
+				else {
+					if (carN[i] == 'c') {
+						ans *= 26;
+					} else {
+						ans *= 10;
+					}
+				}
+			}
+		}
+		System.out.println(ans);
 	}
-	
-	
-	
 }
