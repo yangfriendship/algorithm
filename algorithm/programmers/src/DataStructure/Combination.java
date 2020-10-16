31c89ce32411892c5ea1706b82cfc63d548b4e2a
youzheng <yangfriendship@naver.com>
2020-10-16 19:35:08 +0900

    beakjoon_15650

--- algorithm/programmers/src/DataStructure/Combination.java
+++ algorithm/programmers/src/DataStructure/Combination.java
@@ -6,14 +6,15 @@ import java.util.List;
 public class Combination {
 	static List<List<Integer>> list = new ArrayList<>();
 	static List<List<Integer>> oppt = new ArrayList<>();
+
 	public static void main(String[] args) {
 		int n = 4;
 		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
 		boolean[] visited = new boolean[arr.length];
 		combination(arr, visited, 0, arr.length, 4);
 		System.out.println(list.size());
-		print(list,oppt);
-		
+		print(list, oppt);
+
 	}
 
 	// 백트래킹 사용
@@ -32,29 +33,29 @@ public class Combination {
 	}
 
 	// 배열 출력
-	static void print(List<List<Integer>> list,List<List<Integer>> oppt) {
-		
+	static void print(List<List<Integer>> list, List<List<Integer>> oppt) {
+
 		for (int i = 0; i < list.size(); i++) {
-			
+
 			for (int j = 0; j < list.get(i).size(); j++) {
-				System.out.print(list.get(i).get(j)+" ");
+				System.out.print(list.get(i).get(j) + " ");
 			}
 			System.out.print(" : ");
 			for (int j = 0; j < list.get(i).size(); j++) {
-				System.out.print(oppt.get(i).get(j)+" ");
+				System.out.print(oppt.get(i).get(j) + " ");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 	static void toIntList(int[] arr, boolean[] visit, int n) {
 		List<Integer> result = new ArrayList<Integer>();
-		List<Integer> result2= new ArrayList<Integer>();
+		List<Integer> result2 = new ArrayList<Integer>();
 		for (int i = 0; i < visit.length; i++) {
 			if (visit[i]) {
 				result.add(arr[i]);
-			}else if(!visit[i]) {
+			} else if (!visit[i]) {
 				result2.add(arr[i]);
 			}
 		}
